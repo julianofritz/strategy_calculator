39ee57f34220d158af8e39705224907b1226898c
Juliano <juliano.basso@avec.app>
2022-03-02 21:30:59 -0300

    create a simple pipeline

--- src/CustomClass/Calculator.php
+++ src/CustomClass/Calculator.php
@@ -18,15 +18,11 @@ class Calculator implements CalculatorInterface
 
     public function execute()
     {
-        $a = 1;
-$b = $a;
-$asfadsfadsfadsfadsfadsfadsfsfd = 0;
         try {
             return $this->operator->calculate();
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
-
     }
 
 }
